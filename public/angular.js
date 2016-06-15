717f0241d1110637923e9e06da07de8781040ff2
Derek Jacobi <derekjacobi@gmail.com>
2016-06-14 22:21:17 -0400

    styling, hiding, remove all

--- public/angular.js
+++ public/angular.js
@@ -9,10 +9,12 @@ function ToDoController($scope){
   //add task to to-do list
   $scope.addTask = function(task){
     //make sure a task was entered, add to tasks array, clear task input field in html
-    if (task !== undefined) {
+    if (task !== undefined && $scope.tasks.indexOf(task) === -1) {
       $scope.tasks.push(task);
       $('#task-entry').val('');
       $scope.count++;
+    } else if ($scope.tasks.indexOf(task) != -1){
+      alert('Task already exists!');
     } else {
       alert('Please enter a task!');
     }
@@ -25,9 +27,13 @@ function ToDoController($scope){
     $scope.count--;
   };
 
+  $scope.removeAllTasks = function(){
+    $scope.tasks = [];
+  };
+
 }
 
-    ToDoController.$inject = ['$scope'];
+  ToDoController.$inject = ['$scope'];
 
 angular
   .module('toDo', [])
